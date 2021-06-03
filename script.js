f05d2e39852e086bbe840082dbfaa0429781fc1b
Vitor Silva Mendes <vitorsmendes0909@gmail.com>
2021-06-03 12:52:12 -0300

    Site quase finalizado

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ document.querySelector (".hamburguer").addEventListener("click", function(){
     document.querySelector(".conteiner").classList.toggle("show-menu");
 
 });
+
+
+document.querySelector ('.menu-link1').addEventListener("click",function(){
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
+
+document.querySelector ('.menu-link2').addEventListener("click",function(){
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
+
+document.querySelector ('.menu-link3').addEventListener("click",function(){
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
+
+document.querySelector ('.menu-link4').addEventListener("click",function(){
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
